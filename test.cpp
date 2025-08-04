a1f9db07c70c982dfd4b6112871998f075fb66df
Abu-Saeed-7 <abusaeidrifat@gmail.com>
2025-08-04 16:48:16 +0600

    Update test.cpp
    new template

--- test.cpp
+++ test.cpp
@@ -1,9 +1,38 @@
+                            /*in the name of almighty ALLAH*/
+ 
 #include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define int long long
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define pb  push_back
+#define ppb pop_back
+#define ff  first
+#define ss  second
 #define endl '\n'
-const int MOD = 1000000007;
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define MOD 1000000007 
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using multi_pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 //***********************************START*******************************************//
 
 void solve()
@@ -15,7 +44,7 @@ void solve()
 int32_t main(){ fast
     
     int32_t t = 1, i;
-    // cin >> t;
+      cin >> t;
     for(i = 1; i <= t; i++)
     {
        // cout << "Case " << i << ": ";
